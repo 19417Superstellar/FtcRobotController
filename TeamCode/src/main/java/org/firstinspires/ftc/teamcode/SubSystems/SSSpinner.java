097e3aedf7f0b74b1f2741b4f327048a243b400e
Shloka Gupta <iamshlokagupta@gmail.com>
2021-10-12 19:47:38 -0500

    Added functions for spinner subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SSSpinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SSSpinner.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.SubSystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -35,12 +36,13 @@ public class SSSpinner {
     public SSSPINNER_MOTOR_STATE SSSpinnerMotorState = SSSPINNER_MOTOR_STATE.SPINNER_STOPPED;
 
     public double SSSpinnerMotorPower1 = 0.95;//0.9;
-    public double SSSpinner1MotorPower2 = 0.8;
+
 
     public enum SSSPINNER_BUTTON_STATE {
         ON,
         OFF
     }
+
     public SSSPINNER_BUTTON_STATE SSSpinnerButtonState;
 
     public SSSpinner(HardwareMap hardwareMap) {
@@ -48,65 +50,51 @@ public class SSSpinner {
 
     }
 
-    public void SSSpinner(){
+    public void SSSpinner() {
 
     }
 
     /**
-     * runIntakeMotor checks if the intake is not running and runs the intake
+     * startForwardSSSPinnerMotor checks if the intake is not running and runs the spinner
      */
-    /*public void startForwardsubsystem1Motor() {
-        if(subsystem1 != SUBSYSTEM1_MOTOR_STATE.STATE1) {
-            runSubsystem1Motor(DcMotor.Direction.REVERSE, subsystem1MotorPower1);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE1;
+    public void startForwardSSSPinnerMotor() {
+        if (SSSpinnerMotorState == SSSPINNER_MOTOR_STATE.SPINNER_FORWARD) {
+            runSSSpinnerMotor(DcMotor.Direction.REVERSE, SSSpinnerMotorPower1);
+            SSSpinnerMotorState = SSSPINNER_MOTOR_STATE.SPINNER_STOPPED;
         }
-    }*/
+    }
 
     /**
      * stopIntakeMotor checks if the intake has stopped and if its not, it sets the intake power to 0
      * and sets intakeMotorState to INTAKE_MOTOR_STATE.STOPPED
      */
-    /*public void stopSubsystem1Motor() {
-        if(subsystem1MotorState != SUBSYSTEM1_MOTOR_STATE.STATE2) {
-            runSubsystem1Motor(DcMotor.Direction.FORWARD, 0.0);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE2;
-       }
-    }*/
+    public void stopSubsystem1Motor() {
+        if (SSSpinnerMotorState == SSSPINNER_MOTOR_STATE.SPINNER_FORWARD) {
+            runSSSpinnerMotor(DcMotor.Direction.FORWARD, 0.0);
+            SSSpinnerMotorState = SSSPINNER_MOTOR_STATE.SPINNER_STOPPED;
+        }
+    }
 
     /**
      * reverseIntakeMotor checks if the intake is not reversing, and sets the intake motor to FORWARD, then also
      * ets intake motor state to REVERSING
      */
-   /* public void startReverseSubsystem1Motor() {
-        if(subsystem1MotorState != SUBSYSTEM1_MOTOR_STATE.STATE3) {
-            runSubsystem1Motor(DcMotor.Direction.FORWARD, subsystem1MotorPower2);
-            subsystem1MotorState = SUBSYSTEM1_MOTOR_STATE.STATE3;
+    public void startReverseSubsystem1Motor() {
+        if (SSSpinnerMotorState != SSSPINNER_MOTOR_STATE.SPINNER_REVERSED) {
+            runSSSpinnerMotor(DcMotor.Direction.REVERSE, SSSpinnerMotorPower1);
+            SSSpinnerMotorState = SSSPINNER_MOTOR_STATE.SPINNER_REVERSED;
         }
-    }*/
+    }
 
-    /*private void runSubsystem1Motor(DcMotor.Direction direction, double power){
-        subsystem1Motor.setDirection(direction);
-        subsystem1Motor.setPower(power);
-    }*/
+    private void runSSSpinnerMotor(DcMotor.Direction direction, double power) {
+        SSSpinnerMotor.setDirection(direction);
+        SSSpinnerMotor.setPower(power);
+    }
 
-    /**
-     * set Intake gripper position to hold.. to ensure intake is within robot dimensions at start
-     */
-    /*public void setIntakeReleaseHold(){
-        subsystem1Servo.setPosition(SUBSYSTEM1_SERVO_LEVEL1);
-    }*/
+    //Returns the state of the Spinner Motor
+    public SSSpinner.SSSPINNER_MOTOR_STATE getSSSpinnerMotorState() {
+        return SSSpinnerMotorState;
+    }
 
-    /**
-     * set Intake gripper position to release
-     */
-    /*public void setIntakeReleaseOpen(){
-        subsystem1Servo.setPosition(SUBSYSTEM1_SERVO_LEVEL2);
-    }*/
 
-    /**
-     * Returns Intake motor state
-     */
-    /* SUBSYSTEM1_MOTOR_STATE getSubsystemMotorState() {
-        return subsystem1MotorState;
-    }*/
 }
