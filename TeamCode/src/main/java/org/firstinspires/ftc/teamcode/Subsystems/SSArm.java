ae1a63e38c69274a999c8212e7d0d097cbf266f8
Aarushi <26banera@elmbrookstudents.org>
2022-10-27 19:28:40 -0500

    Started coding arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SSArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SSArm.java
@@ -1,9 +1,212 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorEx;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
 public class SSArm {
-    public SSArm(HardwareMap hardwareMap) {
+
+
+    public DcMotorEx armMotorRight;
+    public DcMotorEx armMotorLeft;
+    //add 2 motors left and right
+    //Gobilda (enter motor details later
+    //Encoder count : enter details to be done
+
+
+    public enum ARM_POSITION {
+       ARM_POSITION_INTAKE,
+        ARM_POSITION_GROUND,
+        ARM_POSITION_MID,
+        ARM_POSITION_LOW,
+        ARM_POSITION_HIGH
 
     }
+
+
+    // Encoder values for 5203 Gobilda 312rpm motor yyyy encoder counts / revolution
+    //5203 Series Yellow Jacket Planetary Gear Motor (19.2:1 Ratio, 24mm Length 8mm REX Shaft, 312 RPM, 3.3 - 5V Encoder)
+    //Encoder Resolution: 537.7 PPR at the Output Shaft
+    //https://www.gobilda.com/5203-series-yellow-jacket-planetary-gear-motor-19-2-1-ratio-24mm-length-8mm-rex-shaft-312-rpm-3-3-5v-encoder/
+    public static double ENCODER_VALUE = 537.7;
+    public static int baselineEncoderCount = 0;
+   // public static int ARM_BASELINE_POSITION_COUNT = 0;
+    public static int ARM_PICKUP_POSITION_COUNT = 650;//  TODO : Determine by experimentation
+    //Use testarm teleop to verify if armMotorLeft and armMotorRight use the same encoder values
+    //if not, add different constants for the two motors
+    //public static int ARM_CUP_POSITION_COUNT = 375;//  TODO : Determine by experimentation
+    public static int ARM_PARKED_POSITION_COUNT = 0;//  TODO : Determine by experimentation
+    public static int ARM_LOW_POSITION_COUNT=0;
+    public static int ARM_MID_POSITION_COUNT=0;
+    public static int ARM_HIGH_POSITION_COUNT=0;
+    public static int ARM_INTAKE_POSITION_COUNT=0;
+    public static int ARM_GROUND_POSITION_COUNT=0;
+    //add count positions for different junctions
+    //MAX 2200
+
+
+    public static double POWER_ARM_UP = 0.2;
+
+
+    public ARM_POSITION armPosition = ARM_POSITION.ARM_POSITION_INTAKE;
+    public ARM_POSITION previousPosition = ARM_POSITION.ARM_POSITION_INTAKE;
+
+
+    public int armPositionCount = ARM_PICKUP_POSITION_COUNT;
+
+
+
+    public SSArm(HardwareMap hardwareMap) {
+        armMotorLeft = hardwareMap.get(DcMotorEx.class,"arm_motor_left");
+        armMotorRight = hardwareMap.get(DcMotorEx.class,"arm_motor_right");
+        initArm();
+    }
+
+    /**
+     * Initialization for the Arm
+     */
+    public void initArm(){
+        resetArm();
+        turnArmBrakeModeOff();
+        armMotorLeft.setPositionPIDFCoefficients(5.0);
+        armMotorLeft.setTargetPosition(ARM_PARKED_POSITION_COUNT);
+        armMotorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        armMotorRight.setPositionPIDFCoefficients(5.0);
+        armMotorRight.setTargetPosition(ARM_PARKED_POSITION_COUNT);
+        armMotorRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        armPosition = ARM_POSITION.ARM_POSITION_INTAKE;
+    }
+
+
+    public void resetArm(){
+        //DcMotor.RunMode runMode = elevatorMotor.getMode();
+        armMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //armMotor.setMode(runMode);
+    }
+
+    /**
+     * Method to set Elevator brake mode to ON when Zero (0.0) power is applied. <BR>
+     * To be used when arm is above groundlevel
+     * setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+     */
+    public void turnArmBrakeModeOn(){
+        armMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    /**
+     * Method to set Elevator brake mode to OFF when Zero (0.0) power is applied. <BR>
+     * To be used when arm is on groundlevel or blockLevel[0]
+     * setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+     */
+    public void turnArmBrakeModeOff(){
+        armMotorLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        armMotorRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+
+    }
+
+    public boolean runArmToLevelState = false;
+    public double motorPowerToRun = POWER_ARM_UP;
+
+    /**
+     * Method to run motor to set to the set position
+     */
+    public void runArmToLevel(double power){
+        armMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (runArmToLevelState /*|| armMotorLeft.isBusy()*/ ){
+            armMotorLeft.setPower(power);
+            runArmToLevelState = false;
+        } else {
+            armMotorLeft.setPower(0.0);
+        }
+        armMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (runArmToLevelState /*|| armMotorRight.isBusy()*/ ){
+            armMotorRight.setPower(power);
+            runArmToLevelState = false;
+        } else {
+            armMotorRight.setPower(0.0);
+        }
+    }
+
+
+    /**
+     * Move Elevator to level0 Position
+     */
+    public void moveArmIntake() {
+        turnArmBrakeModeOn();
+        armPositionCount = ARM_PICKUP_POSITION_COUNT + baselineEncoderCount;
+        armMotorLeft.setTargetPosition(armPositionCount);
+        armMotorRight.setTargetPosition(armPositionCount);
+        motorPowerToRun = POWER_ARM_UP;
+        runArmToLevelState = true;
+        armPosition = ARM_POSITION.ARM_POSITION_INTAKE;
+    }
+
+    public void moveArmLow() {
+        turnArmBrakeModeOn();
+        armPositionCount = ARM_LOW_POSITION_COUNT + baselineEncoderCount;
+        armMotorLeft.setTargetPosition(armPositionCount);
+        armMotorRight.setTargetPosition(armPositionCount);
+        motorPowerToRun = POWER_ARM_UP;
+        runArmToLevelState = true;
+        armPosition = ARM_POSITION.ARM_POSITION_LOW;
+    }
+
+    public void moveArmGround() {
+        turnArmBrakeModeOff();
+        armPositionCount = ARM_PARKED_POSITION_COUNT + baselineEncoderCount;
+        armMotorLeft.setTargetPosition(armPositionCount);
+        armMotorRight.setTargetPosition(armPositionCount);
+        motorPowerToRun = POWER_ARM_UP;
+        runArmToLevelState = true;
+        armPosition = ARM_POSITION.ARM_POSITION_GROUND;
+    }
+
+    public void moveArmMid() {
+        turnArmBrakeModeOn();
+
+        armPositionCount = ARM_MID_POSITION_COUNT + baselineEncoderCount + 30;
+        armMotorLeft.setTargetPosition(armPositionCount);
+        armMotorRight.setTargetPosition(armPositionCount);
+        motorPowerToRun = POWER_ARM_UP;
+        runArmToLevelState = true;
+        armPosition = ARM_POSITION.ARM_POSITION_MID;
+    }
+    public void moveArmHigh() {
+        turnArmBrakeModeOn();
+
+        armPositionCount = ARM_HIGH_POSITION_COUNT + baselineEncoderCount + 30;
+        armMotorLeft.setTargetPosition(armPositionCount);
+        armMotorRight.setTargetPosition(armPositionCount);
+        motorPowerToRun = POWER_ARM_UP;
+        runArmToLevelState = true;
+        armPosition = ARM_POSITION.ARM_POSITION_HIGH;
+    }
+
+
+
+
+
+
+    //function to determinie POWER_WITH_CARGO or POWER_WITH_NO_CARGO
+
+    public ARM_POSITION getArmPosition() {
+
+        return armPosition;
+    }
+
+
+
+    public int currentEncoderValue(){
+
+        return armMotorLeft.getCurrentPosition();
+        return armMotorRight.getCurrentPosition()
+
+    }
+
 }
