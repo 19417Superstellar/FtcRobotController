8899736a987bdb64d2f44de1f5690c78c42ab6a0
vaish <26mayyav@elmbrookstudents.org>
2023-12-04 18:36:17 -0600

    Added tflite

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/VisionTfod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/VisionTfod.java
@@ -25,13 +25,19 @@ public class VisionTfod {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
+    private static final String TFOD_MODEL_ASSET = "model_20231202_184741.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+  //  private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-            "Pixel",
+            "RedTowerCenter",
+            "RedTowerLeft",
+            "RedTowerRight",
+            "BlueTowerCenter",
+            "BlueTowerLeft",
+            "BlueTowerRight"
+
     };
 
     //Vision parameters
@@ -61,16 +67,16 @@ public class VisionTfod {
                 // choose one of the following:
                 //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
                 //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
+                .setModelAssetName(TFOD_MODEL_ASSET)
                 //.setModelFileName(TFOD_MODEL_FILE)
 
                 // The following default settings are available to un-comment and edit as needed to
                 // set parameters for custom models.
-                //.setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
+                .setModelLabels(LABELS)
+                .setIsModelTensorFlow2(true)
+                .setIsModelQuantized(true)
+                .setModelInputSize(300)
+                .setModelAspectRatio(16.0 / 9.0)
 
                 .build();
 
