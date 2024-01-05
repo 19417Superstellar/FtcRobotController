7c9e61fb11f1d49cc12162c7070641de1d5301b3
Ardaman Grewal <ardaman.grewal@froogelpm.com>
2024-01-04 21:54:32 -0600

    Adjusted launcher servo values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SSRocketLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/SSRocketLauncher.java
@@ -28,8 +28,8 @@ public class SSRocketLauncher {
     //MAX 2200
 
     //change values
-    public static double LAUNCHER_OPEN_POSITION = 0.2;
-    public static double LAUNCHER_CLOSE_POSITION = 0.5;
+    public static double LAUNCHER_OPEN_POSITION = 0.4;
+    public static double LAUNCHER_CLOSE_POSITION = 0.6;
 
     public Telemetry telemetry;
     public SSRocketLauncher(HardwareMap hardwareMap, Telemetry telemetry) {
